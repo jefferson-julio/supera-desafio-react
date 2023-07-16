5cb43b3f0343b5ed22c01a20f630380de9e2e0b1
Telérico Jones <snhr.jj@gmail.com>
2023-07-16 02:11:17 -0300

    test: criar task cypress para mockar models

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,15 @@
 import { defineConfig } from "cypress";
+import { mock } from 'intermock'
+import fs from 'fs'
+
+const generateFakeModel = (modelName: string) => {
+  const modelFile = `./src/app/model/${modelName}.ts`
+  const mockedData = mock({
+    files: [[modelFile, fs.readFileSync(modelFile).toString()]],
+    interfaces: [modelName]
+  })
+  return mockedData;
+}
 
 export default defineConfig({
   component: {
@@ -6,5 +17,13 @@ export default defineConfig({
       framework: "next",
       bundler: "webpack",
     },
+    setupNodeEvents(on, config) {
+      on('task', {
+        generateFakeModel(modelName: string) {
+          console.log('RAMO SIMBORA')
+          return generateFakeModel(modelName)
+        },
+      })
+    }
   },
 });
